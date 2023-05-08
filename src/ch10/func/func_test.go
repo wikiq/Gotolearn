e5336e1ed12a629cbe06849df68d7be58737713e
wikiq <qiws@msn.com>
2023-05-08 18:37:02 +0800

    Ch9-push

---
+++ src/ch10/func/func_test.go
@@ -0,0 +1,36 @@
+package _func
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Println("time spent", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+// timeSpent是一个干嘛的函数？
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
+}
+
+//结果每次都不一样，因为rand.Intn()是随机数
