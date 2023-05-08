f2605868fce54009ed03d01070a20b27d0432c38
wikiq <qiws@msn.com>
2023-05-08 22:26:03 +0800

    面向对象编程-行为的定义和实现

--- src/ch11/encapsulation/encap_test.go
+++ src/ch11/encapsulation/encap_test.go
@@ -12,24 +12,24 @@ type Employee struct {
 	Age  int
 }
 
-/*func (e Employee) String() string {
-	fmt.Printf("Address is %x\n", unsafe.Pointer(&e.Name))
-	return fmt.Sprintf("ID:%s-Name:%s-Age:%d", e.Id, e.Name, e.Age)
-}*/
-
-/*func TestStructOperations(t *testing.T) {
-	e := Employee{"0", "Bob", 20}
-	t.Log(e.String())
-}*/
-
 func (e *Employee) String() string {
 	fmt.Printf("Address is %x\n", unsafe.Pointer(&e.Name))
-	return fmt.Sprintf("ID:%s-Name:%s-Age:%d", e.Id, e.Name, e.Age)
+	return fmt.Sprintf("ID:%s///Name:%s///Age:%d", e.Id, e.Name, e.Age)
 }
 
 func TestCreateEmployeeObj(t *testing.T) {
 	e := Employee{"0", "Bob", 20}
 	t.Log(e.String())
+}
+
+func (e Employee) String1() string {
+	fmt.Printf("Address is %x\n", unsafe.Pointer(&e.Name))
+	return fmt.Sprintf("ID:%s//Name:%s//Age:%d", e.Id, e.Name, e.Age)
+}
+
+func TestStructOperations(t *testing.T) {
+	e := Employee{"0", "Bob", 20}
+	t.Log(e.String1())
 }
 
 //上面这是在给e赋值，下面这是在调用e的方法
@@ -60,3 +60,14 @@ func TestCreateEmployeeObj(t *testing.T) {
 //用指针传递
 //比如我现在不需要修改原来的值，我应该用哪种传递方式？
 //用值传递
+
+//值传递和指针传递哪个性能更好？
+//值传递更好，因为指针传递需要解引用，而值传递不需要解引用
+//为什么指针传递需要解引用？
+//因为指针传递是传递的地址，所以需要解引用
+//为什么值传递不需要解引用？
+//因为值传递是传递的值，所以不需要解引用
+//哪种需要复制？
+//值传递需要复制，指针传递不需要复制
+//为什么值传递需要复制？
+//因为值传递是传递的值，所以需要复制
