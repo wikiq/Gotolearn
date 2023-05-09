40445551e802a1a15fd4e587189ded673776a0fe
wikiq <qiws@msn.com>
2023-05-09 17:13:14 +0800

    document-fmt

--- document/package/package.go
+++ document/package/package.go
@@ -9,5 +9,27 @@ import (
 func main() {
 	fmt.Println("A Number:", rand.Intn(10))
 	//rand.Intn(10) 生成一个0-10的随机数
-	fmt.Printf("Now you have %g problems. \n", math.Sqrt(7))
+	fmt.Printf("Now you have %g problems. \n", math.Sqrt(9))
+	//%g 以浮点数形式输出
+	//%f 以浮点数形式输出
+	//%d 以整数形式输出
+	//%s 以字符串形式输出
+	//%t 以布尔值形式输出
+	//%v 以默认格式输出
+	//%T 输出变量的类型
+	//%p 输出变量的内存地址
+	//%p 以十六进制形式输出
+	//%b 以二进制形式输出
+	//%o 以八进制形式输出
+	//%x 以十六进制形式输出
+	//%X 以十六进制形式输出（大写字母）
+	//%U 以Unicode格式输出
+	//%e 以科学计数法输出
+	//%E 以科学计数法输出（大写字母）
+	//%c 以字符形式输出
+	//%q 以带引号的字符串形式输出
+	//%U 以Unicode格式输出
+	//%t 以布尔值形式输出
+	//%v 以默认格式输出
+	//%T 输出变量的类型
 }
