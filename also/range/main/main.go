1da406b83ae4735b024d78ffe9f464bd5aec6ba3
v_wensqi <v_wensqi@tencent.com>
2025-01-16 16:43:23 +0800

    modified:   also/range/main/main.go 	modified:   also/slice_append/main/main.go

--- also/range/main/main.go
+++ also/range/main/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+	slice := []int{10, 20, 30, 40}
+	// 迭代每一个元素，并显示其值
+	for index, value := range slice {
+		fmt.Printf("Index: %d Value: %d\n", index, value)
+	}
+}
