880c76622e5872209be2b45e75a391e56fed1bbb
v_wensqi <v_wensqi@tencent.com>
2025-01-16 16:55:06 +0800

    modified:   also/main 	modified:   also/range/main/main.go

--- also/range/main/main.go
+++ also/range/main/main.go
@@ -1,11 +1,21 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
+	// slice := []int{10, 20, 30, 40}
+	// // 迭代每一个元素，并显示其值
+	// for index, value := range slice {
+	// 	fmt.Printf("Index: %d Value: %d\n", index, value)
+	// }
+
+	gopath := os.Getenv("GOPATH")
+    fmt.Println("GOPATH:", gopath)
+
 	slice := []int{10, 20, 30, 40}
-	// 迭代每一个元素，并显示其值
+	// 迭代每个元素，并显示值和地址
 	for index, value := range slice {
-		fmt.Printf("Index: %d Value: %d\n", index, value)
-	}
+		fmt.Printf("Value: %d Value-Addr: %X ElemAddr: %X\n", value, &value, &slice[index])
+}
 }
