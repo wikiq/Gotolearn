1d12e65dd9374ab8c11c06e1dd137af8c5d3c981
v_wensqi <v_wensqi@tencent.com>
2025-01-17 09:23:48 +0800

    Add map_range function and call it in main for demonstration

--- also/map/main/main.go
+++ also/map/main/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	map_range()
+}
+
+func map_test() {
 	var mapList map[string]int
 	var mapCreated map[string]float64
 	var maspAssigned map[string]int
@@ -17,4 +21,19 @@ func main() {
 	fmt.Printf("Map Assigned at \"two\" is: %d\n", mapList["two"])
 	fmt.Printf("Map Literal at \"ten\" is: %d\n", mapList["ten"])
 
+	// noteFrequency := map[string]float32{
+	// 	"C0": 16.35, "D0": 18.35, "E0": 20.60, "F0": 21.83,
+	// 	"G0": 24.50, "A0": 27.50, "B0": 30.87, "A4": 440}
+	// fmt.Println(noteFrequency)
+}
+
+func map_range() {
+	scene := make(map[string]int)
+
+	scene["route"] = 66
+	scene["brazil"] = 4
+	scene["china"] = 960
+	for key, value := range scene {
+		fmt.Println("Key:", key, "Value:", value)
+	}
 }
