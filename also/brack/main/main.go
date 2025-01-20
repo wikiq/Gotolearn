246993d029c21e55a79a45e00754220e29aff1de
v_wensqi <v_wensqi@tencent.com>
2025-01-20 11:10:24 +0800

    feat: add continue_test function to demonstrate labeled continue in Go

--- also/brack/main/main.go
+++ also/brack/main/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func break_test() {
 OuterLoop:
+	//这个OuterLoop是标签,可以在break的时候指定跳出到哪个标签break_test
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 5; j++ {
+			//第一次执行时，i=0,j=2时，跳出外层循环
 			switch j {
 			case 2:
 				fmt.Println(i, j)
@@ -18,7 +20,21 @@ OuterLoop:
 	}
 }
 
+func continue_test() {
+OuterLoop2:
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 5; j++ {
+			switch j {
+			case 2:
+				fmt.Println(i, j)
+				continue OuterLoop2
+			}
+		}
+	}
+}
+
 func main() {
 	break_test()
+	continue_test()
 
 }
