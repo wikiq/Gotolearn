f3b260bce0acfe02e8c83cfabc6051530f4415a1
v_wensqi <v_wensqi@tencent.com>
2025-02-08 15:41:37 +0800

    refactor: 重构 main 函数逻辑，新增 RPCError 结构体及方法

--- also/struct/struct_tag.go
+++ also/struct/struct_tag.go
@@ -1,32 +1,61 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-func main() {
-	article := Article{
-		Title:    "Golang for Qiws",
-		Desc:     "desc123123",
-		Content:  "content1231231",
-		Author:   "author12312312",
-		UserName: "admin123",
-		password: "password123",
-	}
-	// json.Marshal(article)
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(jsonData))
+// func main() {
+// 	article := Article{
+// 		Title:    "Golang for Qiws",
+// 		Desc:     "desc123123",
+// 		Content:  "content1231231",
+// 		Author:   "author12312312",
+// 		UserName: "admin123",
+// 		PassWord: "password123",
+// 	}
+// 	// json.Marshal(article)
+// 	jsonData, err := json.Marshal(article)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(string(jsonData))
+// }
+
+// type Article struct {
+// 	Title    string `json:"title"`
+// 	Desc     string `json:"desc"`
+// 	Content  string `json:"content"`
+// 	Author   string `json:"author"`
+// 	UserName string `json:"userName"`
+// 	PassWord string `json:"password"`
+// }
+
+type error interface {
+	Error() string
 }
 
-type Article struct {
-	Title    string
-	Desc     string
-	Content  string
-	Author   string
-	UserName string
-	password string
+type RPCError struct {
+	Code    int64
+	Message string
+}
+
+func (e *RPCError) Error() string {
+	return fmt.Sprintf("code: %d, message: %s", e.Code, e.Message)
+}
+
+func main() {
+	var rpcErr error = NewRPCError(400, "unknown err") // typecheck1
+	err := AsErr(rpcErr)                               // typecheck2
+	println(err)
+}
+
+func NewRPCError(code int64, msg string) error {
+	return &RPCError{ // typecheck3
+		Code:    code,
+		Message: msg,
+	}
+}
+
+func AsErr(err error) error {
+	return err
 }
